aa7c836a7846dd2a6f5d22fc1bc3fd4554dbf10b
Chetoui <Chetoui@DESKTOP-6FB5I5F>
2024-11-10 23:06:36 +0100

    Add project files.

---
+++ Guess  3-Letter Password/Guess  3-Letter Password.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+using namespace std;
+
+string ReadPassword()
+{
+	string Password;
+	cout << "Please enter the password (ONLY CAPITAL LETTER): ";
+	cin >> Password;
+
+	return Password;
+}
+
+void PrintAllWordsFromAAAToZZZ(string Password)
+{
+	string word = "";
+	int counter = 0;
+	for (int i = 65; i <= 90; i++)
+	{
+
+		for (int j = 65; j <= 90; j++)
+		{
+			for (int k = 65; k <= 90; k++)
+			{
+				word = "";
+				word += char(i);
+				word += char(j);
+				word += char(k);
+				counter++;
+				cout << "Trial [" << counter << "] : " << word << endl;;
+				if(word==Password)
+				{
+					cout << "Password is " << word << endl;
+					cout << "found after Trial(s) [" << counter << "]";
+					return;
+				}
+			}
+			
+		}
+		
+	}
+	cout << "Password not found within AAA to ZZZ range." << endl;
+}
+
+
+int main()
+{	
+	 PrintAllWordsFromAAAToZZZ(ReadPassword());
+	return 0;
+
+}
